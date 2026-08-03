2db660ad6be665737cf4184b1d8334aabce4ca42
likithmanoj <likithmanojm@gmail.com>
2026-08-04 00:53:33 +0530

    Add AdministratorAccess analyzer for attached managed policies

--- nhi/risk/risk.py
+++ nhi/risk/risk.py
@@ -1,5 +1,6 @@
 from nhi.services.inventory import create_inventory
 from nhi.risk.analyze_policy import analyze_policy
+from nhi.risk.analyze_admin_access import analyze_admin_access
 
 response = create_inventory()
 
@@ -12,13 +13,16 @@ findings = []
 for user in users:
     findings.extend(analyze_policy(user['AttachedPolicies'], "User", user['UserName']))    
     findings.extend(analyze_policy(user['InlinePolicies'], "User", user['UserName']))
+    findings.extend(analyze_admin_access(user['AttachedPolicies'], "User", user['UserName']))
 
 for group in groups:
     findings.extend(analyze_policy(group['AttachedPolicies'], "Group", group['GroupName']))
     findings.extend(analyze_policy(group['InlinePolicies'], "Group", group['GroupName']))
+    findings.extend(analyze_admin_access(group['AttachedPolicies'], "Group", group['GroupName']))
 
 for role in roles:
     findings.extend(analyze_policy(role['AttachedPolicies'],"Role", role['RoleName']))
     findings.extend(analyze_policy(role['InlinePolicies'],"Role", role['RoleName']))
+    findings.extend(analyze_admin_access(role['AttachedPolicies'],"Role", role['RoleName']))
 
 print(findings)
