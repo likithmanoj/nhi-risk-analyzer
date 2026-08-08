380a6a9f91cb0351fccd29bc109ff266433f381b
likithmanoj <likithmanojm@gmail.com>
2026-08-09 00:39:35 +0530

    feat(iam): implement privilege escalation rules IAM_04 through IAM_08

--- nhi/risk/risk.py
+++ nhi/risk/risk.py
@@ -1,6 +1,7 @@
 from nhi.services.inventory import create_inventory
 from nhi.risk.rules.wildcards import analyze_policy, analyze_admin_access
 from nhi.risk.rules.credentials import analyze_stale_access_keys,analyze_unused_keys
+from nhi.risk.rules.privilege_escalation import analyze_passrole_with_wildcard
 
 
 response = create_inventory()
@@ -16,17 +17,23 @@ for user in users:
     findings.extend(analyze_policy(user['InlinePolicies'], "User", user['UserName']))
     findings.extend(analyze_admin_access(user['AttachedPolicies'], "User", user['UserName']))
     access_keys = user.get('AccessKeys', [])
-    findings.extend(analyze_stale_access_keys(user['AccessKeys'], user['UserName']))
-    findings.extend(analyze_unused_keys(user['AccessKeys'], user['UserName']))
+    findings.extend(analyze_stale_access_keys(access_keys, user['UserName']))
+    findings.extend(analyze_unused_keys(access_keys, user['UserName']))
+    findings.extend(analyze_passrole_with_wildcard(user['AttachedPolicies'], "User", user['UserName']))
+    findings.extend(analyze_passrole_with_wildcard(user['InlinePolicies'], "User", user['UserName']))
 
 for group in groups:
     findings.extend(analyze_policy(group['AttachedPolicies'], "Group", group['GroupName']))
     findings.extend(analyze_policy(group['InlinePolicies'], "Group", group['GroupName']))
     findings.extend(analyze_admin_access(group['AttachedPolicies'], "Group", group['GroupName']))
+    findings.extend(analyze_passrole_with_wildcard(group['AttachedPolicies'], "Group", group['GroupName']))
+    findings.extend(analyze_passrole_with_wildcard(group['InlinePolicies'], "Group", group['GroupName']))
 
 for role in roles:
     findings.extend(analyze_policy(role['AttachedPolicies'],"Role", role['RoleName']))
     findings.extend(analyze_policy(role['InlinePolicies'],"Role", role['RoleName']))
     findings.extend(analyze_admin_access(role['AttachedPolicies'],"Role", role['RoleName']))
+    findings.extend(analyze_passrole_with_wildcard(role['AttachedPolicies'], "Role", role['RoleName']))
+    findings.extend(analyze_passrole_with_wildcard(role['InlinePolicies'], "Role", role['RoleName']))
 
 print(findings)
